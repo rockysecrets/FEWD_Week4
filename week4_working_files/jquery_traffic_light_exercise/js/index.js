7c9dd88caf3d6e88392ec02f88d19cd3730c395d
Amanda <AMD@AMD.local>
2016-01-16 15:56:53 +0800

    Traffic Light Exercise

--- week4_working_files/jquery_traffic_light_exercise/js/index.js
+++ week4_working_files/jquery_traffic_light_exercise/js/index.js
@@ -1,1 +1,51 @@
 //Implement the red light using jQuery. Don't forget to add the script tags.
+
+//jquery
+$("#stopButton").click(illuminateRed);
+$("#slowButton").click(illuminateYellow);
+$("#goButton").click(illuminateGreen);
+
+function illuminateRed() {
+  clearLights();
+  $("#stopLight").css("backgroundColor", "red");
+}
+
+
+function illuminateYellow() {
+  clearLights();
+  $("#slowLight").css("backgroundColor", "yellow");
+}
+
+
+function illuminateGreen() {
+  clearLights();
+  $("#goLight").css("backgroundColor", "green");
+}
+
+
+function clearLights() {
+  $("#stopLight").css("backgroundColor", "black");
+  $("#slowLight").css("backgroundColor", "black");
+  $("#goLight").css("backgroundColor", "black");
+}
+
+//vanilla js
+// document.getElementById('stopButton').onclick = illuminateRed;
+
+// document.getElementById('slowButton').onclick = illuminateYellow;
+//
+// function illuminateRed() {
+//   clearLights();
+//   document.getElementById('stopLight').style.backgroundColor = "red";
+// }
+//
+// function illuminateYellow() {
+//   clearLights();
+//   document.getElementById('slowLight').style.backgroundColor = "purple";
+// }
+
+// function clearLights() {
+//   document.getElementById('stopLight').style.backgroundColor = "black";
+//   document.getElementById('slowLight').style.backgroundColor = "black";
+//   document.getElementById('goLight').style.backgroundColor = "black";
+// }
